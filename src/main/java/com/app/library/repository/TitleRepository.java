7f14f904b138fb925ee93bcf58ba464fc171aca8
Piotr Janczak <piotr.janczak1112@gmail.com>
2024-04-27 11:39:41 +0200

    CopyService v0.1

--- src/main/java/com/app/library/repository/TitleRepository.java
+++ src/main/java/com/app/library/repository/TitleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TitleRepository extends CrudRepository<Title, Long> {
@@ -13,6 +14,8 @@ public interface TitleRepository extends CrudRepository<Title, Long> {
 
     @Override
     List<Title> findAll();
+
+    Optional<Title> findById(int id);
 
     @Override
     void deleteById(Long id);
--- src/main/java/com/app/library/service/CopyService.java
+++ src/main/java/com/app/library/service/CopyService.java
@@ -1,12 +1,29 @@
 package com.app.library.service;
 
+import com.app.library.domain.Copy;
+import com.app.library.domain.Title;
 import com.app.library.repository.CopyRepository;
+import com.app.library.repository.TitleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CopyService {
 
     private final CopyRepository copyRepository;
+    private final TitleRepository titleRepository;
+
+    public Copy saveCopy(final Copy copy) {
+        Optional<Title> title = titleRepository.findById(copy.getTitle().getId());
+        title.get().getBookCopies().add(copy);
+        return copyRepository.save(copy);
+    }
+
+    public Copy changeStatus(final Copy copy, String status) {
+        copy.setStatus(status);
+        return copyRepository.save(copy);
+    }
 }
